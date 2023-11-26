ec50feb767d07af3fd0f1d24847266ea07230657
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-11-26 21:23:42 +0200

    add User Schema

--- models/User.js
+++ models/User.js
@@ -4,19 +4,24 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: true,
+    required: [true, ' Please provide a name'],
+    lowercase: true,
     maxLength: [20, 'name cant be longer than 20 characters'],
-    minLength: [2, 'name must have at least 2 characters'],
+    minLength: [3, 'name must have at least 3 characters'],
   },
   email: {
     type: String,
-    required: true,
+    required: [true, ' Please provide an email'],
     trim: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    unique: true,
   },
   password: {
     type: String,
-    required: true,
-    maxLength: [20, 'password cant be longer than 20 characters'],
+    required: [true, ' Please provide a password'],
     minLength: [6, 'password must have at least 6 characters'],
   },
 });
